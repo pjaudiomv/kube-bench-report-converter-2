1fc7e88d1a4d0b85de80868d8fde9659e11a86fb
mlewk <mlewk@amazon.de>
2022-11-15 11:59:45 +0100

    Enforce string handling of values when importing into Excel

--- kube_bench_report_converter/writer.py
+++ kube_bench_report_converter/writer.py
@@ -3,7 +3,7 @@ def write_to_file(findings, output_file_path):
         output_file.write('Id;Category;Subcategory;Rating;Description;Remediation\n')
 
         for anchor, finding in findings.items():
-            output_file.write('{};{};{};{};{};{}\n'.format(anchor, finding['category'], finding['subcategory'],
+            output_file.write('="{}";="{}";="{}";="{}";="{}";="{}"\n'.format(anchor, finding['category'], finding['subcategory'],
                                                            finding['level'], finding['description'],
                                                            finding['remediation']['description'] if finding.get(
                                                                'remediation') else ''))
@@ -13,6 +13,6 @@ def write_to_stdout(findings):
     print('Id;Category;Subcategory;Rating;Description;Remediation')
 
     for anchor, finding in findings.items():
-        print('{};{};{};{};{};{}\n'.format(anchor, finding['category'], finding['subcategory'],
+        print('="{}";="{}";="{}";="{}";="{}";="{}"\n'.format(anchor, finding['category'], finding['subcategory'],
                                            finding['level'], finding['description'],
                                            finding['remediation']['description'] if finding.get('remediation') else ''))
